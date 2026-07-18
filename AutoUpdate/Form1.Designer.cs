2efcab1c164dfa41f5af339fc84d2c2db01900d4
mhmtcode <ankaramehmet82@gmail.com>
2026-07-18 22:35:17 +0200

    v4

--- AutoUpdate/Form1.Designer.cs
+++ AutoUpdate/Form1.Designer.cs
@@ -48,6 +48,7 @@
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
--- AutoUpdate/Form1.cs
+++ AutoUpdate/Form1.cs
@@ -6,5 +6,12 @@ namespace AutoUpdate
         {
             InitializeComponent();
         }
+
+        private  void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+     
     }
 }
